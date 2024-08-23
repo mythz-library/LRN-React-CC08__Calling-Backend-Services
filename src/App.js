f289acc12f9cfad3cd1d5ae5e3656e69ea8ac36f
Ashen Jayarathne <ashenjay01@gmail.com>
2024-08-23 22:34:28 +0530

    Creates basic components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <div>App Component</div>;
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="p-5">
+        <button className="btn btn-primary px-4">Add</button>
+
+        <table class="table mt-5">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>#</th>
+              <td>#</td>
+              <td>#</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
